c7a11ba7417fb65e46d926b79c94b8799bd7be67
Aleksa <aleksaputnik047@gmail.com>
2021-05-02 19:22:32 +0200

    Created project with class library

---
+++ ClassLibrary/BookModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class BookModel
+    {
+        public int Id { get; set; }
+        public string BookName { get; set; }
+        public string AuthorName { get; set; }
+        public string Publisher { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        //List<StudentModel> IssuedTo { get; set; } = new List<StudentModel>();
+        //public DateTime DateBorrowed { get; set; }
+        //public DateTime DateReturned { get; set; }
+    }
+}
---
+++ ClassLibrary/IssueBookModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class IssueBookModel
+    {
+        public int Id { get; set; }
+        public DateTime DateBorrowed { get; set; }
+        public DateTime DateReturned { get; set; }
+        public StudentModel BorrowedBy { get; set; }
+        public BookModel Book { get; set; }
+    }
+}
